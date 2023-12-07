4a77cfa7f4e28a985a61e8126456620fdaa13578
Sebastian Rieger <flex@containerlab-test.tail58cb2.ts.net>
2023-12-07 12:33:03 +0000

    implemented topology adjustments for siblings regarding nodes and links, support for changed clab prefix

--- apps/hello-world.py
+++ apps/hello-world.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import re
 from pygnmi.client import gNMIclient
 
 async def run(config, clab_topology_definition, model, sibling):
@@ -6,12 +7,21 @@ async def run(config, clab_topology_definition, model, sibling):
     username = config['gnmi']['username']
     password = config['gnmi']['password']
 
-    # assume same nodes in each sibling as in real network for now
-    for node in clab_topology_definition['topology']['nodes'].items():
-        # assume clab as the default prefix for now
-        host = "clab-" + clab_topology_definition['name'] + "_" + sibling + "-" + node[0]
-        with gNMIclient(target=(host, port), username=username, password=password, insecure=True) as gc:
-            print("Hello World! for sibling " + sibling + " at " + datetime.now().strftime("%H:%M:%S"))
-            description = "Hello World! for sibling " + sibling + " at " + datetime.now().strftime("%H:%M:%S")
-            result = gc.set(update=[("openconfig:interfaces/interface[name=Ethernet1]", {"config": {"description": description}})])
-            # print("gNMI interface desc set for Ethernet1")
+    for node in model['siblings'][sibling]['clab_topology']['topology']['nodes'].items():
+        # if the sibling has a gnmi-sync config and the node matches the regex
+        if config['siblings'][sibling].get('gnmi-sync') is not None and config['siblings'][sibling]['gnmi-sync'].get('nodes'):
+            if re.fullmatch(config['siblings'][sibling]['gnmi-sync']['nodes'], node[0]):
+                # if the gNMI data for the node's name exists in the model (e.g., not the case for a node that was added to the sibling's topology)
+                if model['nodes'].get(node[0]) is not None:
+                    host = config['clab_topology_prefix'] + "-" + clab_topology_definition['name'] + "_" + sibling + "-" + node[0]
+                    try:
+                        with gNMIclient(target=(host, port), username=username, password=password, insecure=True) as gc:
+                            test_message = "Hello World! for node " + node[0] + " in sibling " + sibling + " at " + datetime.now().strftime("%H:%M:%S")
+                            print("Setting interface description for Ethernet1 on node " + node[0] + " in sibling " + sibling + " to: " + test_message)
+                            try:
+                                result = gc.set(update=[("openconfig:interfaces/interface[name=Ethernet1]", {"config": {"description": test_message}})])
+                                # print(result)
+                            except:
+                                print("Error setting interface desc for Ethernet1 on " + host + " in sibling " + sibling)
+                    except:
+                        print("Error connecting to " + host + " in sibling " + sibling)
