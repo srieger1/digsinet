55d8194cef80cdb5cdebea88f0f4121858410df3
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2023-12-13 12:45:15 +0000

    added te controller, readded multiprocessing starting point

---
+++ controllers/te.py
@@ -0,0 +1,9 @@
+from controllers.controller import Controller
+
+class te(Controller):
+    def __init__(self, config, clab_topology_definition, model, sibling):
+        Controller.__init__(self, config, clab_topology_definition, model, sibling)
+        self.name = "Controller " + __name__ + " for sibling " + sibling
+    
+    def runApps(self):
+        return super().runApps()
