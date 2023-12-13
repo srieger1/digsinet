0ceb8b1a1c2ba74b23b9aef6644638aedc2820f6
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2023-12-13 12:43:54 +0000

    added te controller, readded multiprocessing starting point

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,15 +1,16 @@
 import asyncio
-from multiprocessing import Process
+import multiprocessing
 
 import importlib
 
-class Controller():
+class Controller(multiprocessing.Process):
     apps = dict()
 
     def __init__(self, config, clab_topology_definition, model, sibling):
+        multiprocessing.Process.__init__(self)
+
         self.name = "Controller " + __name__ + " for sibling " + sibling
         self.daemon = False
-        self.target = self.runApps
         self.start()
         print("Process id: " + str(self.pid))
         self.join()
@@ -34,8 +35,9 @@ class Controller():
 
     def runApps(self):
         # get controller for sibling and its apps and run them
+
         for app in self.apps:
             # parallelized execution is suboptimal for now
-            print("=== Running App " + app + " on Controller " + self.name + " for sibling " + self.sibling + "...")
+            print("=== Running App " + app + " on " + self.name + " in pid " + str(self.pid) + " " + str(self.is_alive()) + "...")
             asyncio.run(self.apps[app].run(self.config, self.clab_topology_definition, self.model, self.sibling))
             # self.apps[app].run(self.config, self.clab_topology_definition, self.model, self.sibling)
