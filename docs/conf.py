d87a442adbf3869fcc688630ce31c2a77fa60e02
Leon Lux <leonlux@icloud.com>
2024-05-02 17:36:31 +0200

    add extensions to sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -2,13 +2,15 @@
 
 # Project Information
 project = 'DigSiNet'
-authors = 'Sebastian Rieger <sebastian.rieger@cs.hs-fulda.de>, Leon Lux <leon-niklas.lux@cs.hs-fulda.de>'
+authors = 'Sebastian Rieger <sebastian.rieger@cs.hs-fulda.de>, Leon Lux <leon-niklas.lux@cs.hs-fulda.de>, Jannik Schmitt <jannik.schmitt@cs.hs-fulda.de>'
 
 # General Settings
 extensions = [
     'sphinx.ext.autodoc',  # Include documentation from docstrings
     'sphinx.ext.napoleon', # Support for Google and NumPy style docstrings
-    'sphinx_rtd_theme'
+    'sphinx.ext.autodoc', # Use Python Docstring
+    'sphinx_rtd_theme',
+    "sphinx_tabs.tabs",
 ]
 
 html_theme = "sphinx_rtd_theme"
