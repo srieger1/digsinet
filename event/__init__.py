3070788717552d939e936190e3af501225e39882
leonlux <leonlux@icloud.com>
2024-06-13 13:23:20 +0200

    add interface for event streaming

---
+++ event/__init__.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class EventBroker(ABC):
+    def __init__(self, config, channels: List[str], logger):
+        pass
+
+    @abstractmethod
+    def publish(self, channel: str, data):
+        pass
+
+    @abstractmethod
+    def poll(self, channel: str, timeout):
+        pass
+
+    @abstractmethod
+    def subscribe(self, channel: str):
+        pass
+
+    @abstractmethod
+    def get_sibling_channels(self):
+        pass
+
+    @abstractmethod
+    def new_sibling_channel(self, channel: str):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
