e22f5d438675db33a05c2ef672fdeba26523a25d
Leon Lux <leonlux@icloud.com>
2024-06-14 10:06:34 +0200

    move EventBroker from __init__.py to dedicated file

--- event/eventbroker.py
+++ event/eventbroker.py
@@ -1,26 +1,32 @@
+from abc import ABC, abstractmethod
 from typing import List
-from event.eventbroker import EventBroker
 from logging import Logger
 
 
-class KafkaClient(EventBroker):
+class EventBroker(ABC):
     def __init__(self, config, channels: List[str], logger: Logger):
         pass
 
+    @abstractmethod
     def publish(self, channel: str, data):
         pass
 
+    @abstractmethod
     def poll(self, channel: str, timeout):
         pass
 
+    @abstractmethod
     def subscribe(self, channel: str):
         pass
 
+    @abstractmethod
     def get_sibling_channels(self):
         pass
 
+    @abstractmethod
     def new_sibling_channel(self, channel: str):
         pass
 
+    @abstractmethod
     def close(self):
         pass
