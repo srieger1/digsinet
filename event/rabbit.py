e04b8b9b319ab22252b019c4073b9980d438af68
Leon Lux <leonlux@icloud.com>
2024-06-20 19:53:50 +0200

    implement connecting to rabbit mq server

--- event/rabbit.py
+++ event/rabbit.py
@@ -1,11 +1,49 @@
 from typing import List
+
+import pika
+
 from event.eventbroker import EventBroker
 from logging import Logger
+from pika import ConnectionParameters, PlainCredentials
+from pika.exchange_type import ExchangeType
+from config import RabbitSettings
 
 
 class RabbitClient(EventBroker):
-    def __init__(self, config, channels: List[str], logger: Logger):
+    def __init__(self, config: RabbitSettings, channels: List[str], logger: Logger):
+        """
+        Connects to the RabbitMQ Servers specified in config, declaring an exchange
+        and creating a queue for each channel in channels.
+
+        Attributes:
+            config (RabbitSettings): Configuration object containing RabbitMQ settings
+            channels (List[str]): List of channels to connect to
+        """
         super().__init__(config, channels, logger)
+        self.config = config
+        self.channels = channels
+        self.conn_params = ConnectionParameters(host=config.host, port=config.port,
+                                                credentials=PlainCredentials(config.username, config.password, True))
+
+        self._conn = pika.BlockingConnection(self.conn_params)
+        self.mq_chan = self._conn.channel()
+        self.mq_chan.confirm_delivery()
+
+        self.mq_chan.exchange_declare(
+            exchange='digsinet',
+            exchange_type=ExchangeType.direct,
+            passive=False,
+            durable=True,
+            auto_delete=False
+        )
+
+        for channel in self.channels:
+            self.mq_chan.queue_declare(queue=channel, auto_delete=False)
+            self.mq_chan.queue_bind(
+                queue=channel,
+                exchange='digsinet',
+                routing_key=channel
+            )
 
     def publish(self, channel: str, data):
         pass
