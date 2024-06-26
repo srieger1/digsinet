6872fc3955b39464738a5789f685b349b00401ab
Leon Lux <leonlux@icloud.com>
2024-06-26 13:34:29 +0200

    add message type for rabbit mq (error currently missing)

--- message/rabbit.py
+++ message/rabbit.py
@@ -1,11 +1,13 @@
 from message.message import Message
 
 class RabbitMessage(Message):
-    def __init__(self, message):
-        pass
+    def __init__(self, message, delivery_tag=None):
+        super.__init__(message)
+        self.delivery_tag = delivery_tag
+        self.message = message
 
     def error(self):
         pass
 
     def value(self):
-        pass
+        return self.message
