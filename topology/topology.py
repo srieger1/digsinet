0a806f8ac44a6663fa95f4985e332f9c020d094a
Leon Lux <leonlux@icloud.com>
2024-11-11 13:08:03 +0100

    add topology class as abstraction for topologies

---
+++ topology/topology.py
@@ -0,0 +1,29 @@
+from .link import Link
+from .node import Node
+from typing import List
+
+class Topology:
+    def __init__(self):
+        self.nodes: List[Node] = []
+        self.links: List[Link] = []
+
+class TopologyBuilder:
+    def __init__(self) -> None:
+        self._topo = Topology()
+
+    def add_node(self, name: str, kind: str) -> None:
+        self._topo.nodes.append(Node(name, kind, []))
+
+    def add_link(self, node_from: str, node_to: str, interface_from: str, interface_to: str) -> None:
+        # assert
+        assert any(node.name == node_from for node in self._topo.nodes)
+        assert any(node.name == node_to for node in self._topo.nodes)
+        self._topo.links.append(Link(node_from, node_to, interface_from, interface_to))
+
+    def clear(self) -> None:
+        self._topo.nodes = []
+        self._topo.links = []
+
+    def build(self) -> Topology:
+        return self._topo
+
